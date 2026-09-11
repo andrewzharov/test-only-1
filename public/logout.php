6e6d6e7a107f0fb8fe2bc908cacd41ab88757ef6
Andrew <andrewzharov@gmail.com>
2026-09-11 09:28:55 +0300

    Добавил файл завершения сессии авторизованного пользователя

--- public/logout.php
+++ public/logout.php
@@ -1,1 +1,13 @@
 <?php
+
+declare(strict_types=1);
+
+require_once __DIR__ . '/../vendor/autoload.php';
+
+use App\SessionHelper;
+
+SessionHelper::start();
+SessionHelper::logout();
+
+header('Location: /index.php');
+exit;
